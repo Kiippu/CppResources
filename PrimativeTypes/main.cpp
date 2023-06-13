1ae34bbcfe4a3f9caa2f6ed8278ce2680908ed8b
Kory Smith <kosa.smit@gmail.com>
2023-06-13 22:20:37 +1000

    added primative key words descriptors

---
+++ PrimativeTypes/main.cpp
@@ -0,0 +1,11 @@
+/// Syntax keywords  for primative types
+
+// Only Arithmatic & void types exist
+// 1. Arithmetic -> integral & floating point
+//      a. Integral types -> bool, char ,wchar_t, char16_t, char32_t, short, long
+//      b. Floating point -> float & double
+// 2. void is a special type used with pointers & functions
+
+// Type Modifiers   - signed and unsigned
+// Qualifier        - const, volatile, static 
+

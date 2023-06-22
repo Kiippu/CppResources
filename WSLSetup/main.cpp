b7e92fed85b4d9a8c4a9604482f710ef5c789414
Kory Smith <kosa.smit@gmail.com>
2023-06-22 23:21:01 +1000

    swap 2 ints without using another variable

--- WSLSetup/main.cpp
+++ WSLSetup/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+
+void swap(int& a, int& b)
+{
+    std::cout << "stage 1:\n" << "a: " << a << ", b: " << b <<  std::endl; 
+    a = a + b;
+    std::cout << "stage 2:\n" << "a: " << a << ", b: " << b <<  std::endl; 
+    b = a - b;
+    std::cout << "stage 3:\n" << "a: " << a << ", b: " << b <<  std::endl; 
+    a = a - b;
+    std::cout << "stage 4:\n" << "a: " << a << ", b: " << b <<  std::endl; 
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
     std::cout << "hello world" << std::endl; 
+    int i{1};
+    int k{4};
+    swap(i,k);
     return 0;
 }

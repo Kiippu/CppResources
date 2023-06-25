4f182fb890fd87234d4fd9117666e837cf698c71
Kory Smith <kosa.smit@gmail.com>
2023-06-25 14:07:03 +1000

    delegating constructors

--- ClassesAndObjects/RuleOfThree.cpp
+++ ClassesAndObjects/RuleOfThree.cpp
@@ -2,20 +2,26 @@
  * Rule of three
  *  - if one of the three have been user defined the creat all
  *      - constructor, destructor, copy constructor
+ * 
+ * Delegating constructors
+ *  - reduce code redundancy
+ *  - one common constructor
 */
 
 class Integer
 {
 private:
     int * m_int;
+    int m_count;
 public:
-// user defined constructor
-    Integer(){ 
-        m_int = new int(0); 
-    };
-    Integer(int i){ 
+// user defined constructor and delegating constructors
+    Integer()       : Integer(0)    {};
+    Integer(int i)  : Integer(i, 0) {};
+    Integer(int i, int cnt){ 
         m_int = new int(i); 
+        m_count = cnt;
     };
+
 // copy constructor - Deep copy
     Integer(const Integer &other){
         m_int = new int(*other.m_int); 
@@ -36,6 +42,7 @@ int main(int argc, char const *argv[])
     int *p2 = new int(*p);
     Integer i1(5);
     Integer i2(i1);
+    Integer i3(5, 80);
 
     return 0;
 }

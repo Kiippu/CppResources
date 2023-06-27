ced82adc4515b5e227f5c13112012c61f55fad49
Kory Smith <kosa.smit@gmail.com>
2023-06-27 22:32:13 +1000

    move semantics

---
+++ MoveSemantics/MoveSemantics.cpp
@@ -0,0 +1,65 @@
+#include "iostream"
+
+
+class Integer
+{
+private:
+    int * m_pInt;
+public:
+// user defined constructor and delegating constructors
+    Integer(){
+        m_pInt = new int(0); 
+           std::cout << "Integer()" << std::endl;
+    };
+    Integer(int i) { 
+        m_pInt = new int(i); 
+           std::cout << "Integer(int i)" << std::endl;
+    };
+
+// copy constructor - Deep copy
+    Integer(const Integer &other){
+        m_pInt = new int(*other.m_pInt); 
+           std::cout << "Integer(const Integer &other)" << std::endl;
+    };
+// destructor
+    ~Integer(){ 
+        delete m_pInt; 
+           std::cout << "~Integer()" << std::endl;
+    };
+
+/// Move Constructor
+    Integer(Integer &&other)
+    {
+        std::cout << "Integer(Integer &&other)" << std::endl;
+        m_pInt = other.m_pInt;
+        other.m_pInt = nullptr;
+    }
+
+    void setValue(int x)
+    {
+        *m_pInt = x; 
+    }
+    int getValue() const
+    {
+        return *m_pInt; 
+    }
+};
+
+
+Integer Add(const Integer &a, const Integer &b)
+{
+    // create 1 object
+    Integer temp;
+    temp.setValue(a.getValue() + b.getValue());
+    // move temp 
+    return temp;
+}
+
+int main(int argc, char const *argv[])
+{
+    // create 2 objects
+    Integer a(1), b(3);
+    // use moved temp and getValue form it not a copy
+    a.setValue(Add(a,b).getValue());
+    return 0;
+}
---
+++ MoveSemantics/RAndLValuesAndReference.cpp
@@ -0,0 +1,26 @@
+
+int Add(int x, int y)
+{
+    return x+y;
+}
+
+int& Transform(int &x)
+{
+    x *=x;
+    return x;
+}
+
+int main(int argc, char const *argv[])
+{
+    // R-Value refernce will bind to a R-value 
+    int &&r1 = 10;
+    int &&r2 = Add(3,2);
+
+    int x = 0;
+    // does not compile as Right value reference can not take L value
+    // int &&r3 = x;
+
+    // L-value reference will always bind to a L-value 
+    int &ref = x;
+    return 0;
+}

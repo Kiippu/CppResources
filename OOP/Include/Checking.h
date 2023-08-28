c33c311e08c559f0026c13b694232988fb59e45d
Kory Smith <kosa.smit@gmail.com>
2023-08-28 14:13:50 +1000

    inherit base constructor

--- OOP/Include/Checking.h
+++ OOP/Include/Checking.h
@@ -5,7 +5,9 @@ class Checking : public Account
 {
 private:
 public:
-    Checking(const std::string &name, float balance);
+    //Checking(const std::string &name, float balance);
+    // inherited base class constructor
+    using Account::Account;
     
     void Withdraw(float amount);
 };

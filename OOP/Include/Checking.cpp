cdb891461558b26e022e760751663ed24d703371
Kory Smith <kosa.smit@gmail.com>
2023-08-28 14:14:13 +1000

    inherit base class methods

--- OOP/Include/Checking.cpp
+++ OOP/Include/Checking.cpp
@@ -1,14 +1,11 @@
 #include "Checking.h"
 
-Checking::Checking(const std::string &name, float balance)
-    : Account(name, balance)
-{
-}
+// s
 
 void Checking::Withdraw(float amount)
 {
     if((m_balance - amount) >= 50.f)
-        m_balance -= amount;
+        Account::Withdraw(amount);
     else
         std::cout << "Denied - Balance will fall below limit" << std::endl;
 }

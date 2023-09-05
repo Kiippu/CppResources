c6f326e1d6be7d7c6b8318b70b7d09263a58fdfa
Kory Smith <kosa.smit@gmail.com>
2023-09-05 19:49:52 +1000

    File I/O error handling

--- .vscode/FileInputOutput/FileInputOutput.cpp
+++ .vscode/FileInputOutput/FileInputOutput.cpp
@@ -6,6 +6,11 @@
  * all include fstream header
  * all classes cna be used for text/binary I/O
  * 
+ * stream state flags
+ * goodbit  - no error occured                  good()
+ * badbit   - irrecoverable stream error        bad()
+ * failbit  - I/O opperation failed             fail()
+ * eofbit   - end of file reached during input  eof()
 */
 
 #include <iostream>
@@ -13,6 +18,7 @@
 #include <string>
 
 std::string filename{"data.text"};
+std::string filename_bad{"data1.text"};
 
 void Write()
 {
@@ -27,12 +33,53 @@ void Read()
     // open file with constructor is ok
     //std::ifstream input{filename};
     // can use open() also
-    std::ifstream input;
+    std::ifstream input{filename_bad};
+    // check if a file is open
+    if(!input.is_open())
+    {
+        std::cout << "Could not open file" << std::endl;
+    }
+    // open the file again
+    input.open(filename_bad);
+    if(input.fail())
+    {
+        std::cout << "Could not open file again" << std::endl;
+    }
     input.open(filename);
+    if(input.is_open())
+    {
+        std::cout << "File successfully opened" << std::endl;
+    }
     std::string msg;
     std::getline(input, msg);
     int value{};
     input >> value;
+    input >> value;
+    if(input.eof())
+    {
+        std::cout << "End of file encountered" << std::endl;
+    }
+    if(input.good())
+    {
+        std::cout << "I/O operations successful" << std::endl;
+    }
+    else
+    {
+        std::cout << "some I/O operations failed" << std::endl;
+    } 
+    // clear all bits
+    input.clear();
+    // reset file position to beginning
+    input.seekg(0);
+    if(input.good())
+    {
+        std::cout << "goodbit is set true all file operations have been successful since open" << std::endl;
+    }
+    input.setstate(std::ios::failbit);
+    if(input.fail())
+    {
+        std::cout << "set fail bit true" << std::endl;
+    }
     input.close();
     std::cout << msg << " : " << value << std::endl;
 }

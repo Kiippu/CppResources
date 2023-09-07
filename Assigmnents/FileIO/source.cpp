27e1b68a5ba147221a849911d73739215b2d2ff1
Kory Smith <kosa.smit@gmail.com>
2023-09-07 23:09:19 +1000

    refined binary copy

--- Assigmnents/FileIO/source.cpp
+++ Assigmnents/FileIO/source.cpp
@@ -29,17 +29,30 @@ void CopyFiles(std::string pathStr)
     std::ifstream input(path, std::ios::binary | std::ios::in);
     // get size of binary file
     size_t count{std::filesystem::file_size(path)};
-    count /= sizeof(File);
-    // read out 
-    File file;
-    for (size_t i = 0; i < count; i++)
-    {
-        input.read((char*)&file, sizeof(File));
-        output.write((const char*)&file, sizeof(File));
-    }
+
+    // read out all data at once
+    /// any of the below p* are valid
+    //auto p = std::unique_ptr<char>(new char(count));
+    //char *p = (char*)malloc(count);
+    char *p = new char(count);
+    input.read(p, count);
+    output.write(p, count);
+    delete []p;
     output.close();
     input.close();
 
+    // open copied file and get data
+    input.open(path_output);
+    size_t size = (count / sizeof(File));
+    File file[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        input.read((char*)&file[i], sizeof(File));
+    }
+    input.close();
+    
+    if(true)
+        std::cout << "pause here to check results copy results" << std::endl;
 }
 
 int main(int argc, char const *argv[])

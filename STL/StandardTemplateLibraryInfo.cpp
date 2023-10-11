ab427a9afab5e18c4b34c51f5a14695b93d62231
Kory Smith <kosa.smit@gmail.com>
2023-10-11 21:45:54 +1100

    STL general information

---
+++ STL/StandardTemplateLibraryInfo.cpp
@@ -0,0 +1,18 @@
+/**
+ * Standard Template Library
+ * 
+ * HISTORY:
+ *  - developed at HP 
+ *  - fast reusable containers and algorithms
+ *  - rely heavily on templates
+ * 
+ * CORE:COMPONENTS:
+ * container classes, algorithms and iterators form the core components
+ * container classes represent data
+ * algorithms represents operations on data
+ * iterators serve as the connection between the above
+ * STL also includes classes on concurrency, random numbers, regular expressions, utilities, etc
+ * 
+ * 
+ * 
+*/

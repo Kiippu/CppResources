1fcd4a26ef779182c324deac391a61b54c872eb4
Kory Smith <kosa.smit@gmail.com>
2023-11-14 19:45:16 +1100

    access and modify variants safely

--- C++17Features/include/Variant.h
+++ C++17Features/include/Variant.h
@@ -7,6 +7,16 @@
 #include <iostream>
 #include <string>
 
+struct visitorFn
+{
+    void operator()(const std::string) const
+    {}
+    void operator()(const int) const
+    {}
+    void operator()(const char) const
+    {}
+};
+
 void Variant_main()
 {
     try
@@ -27,6 +37,28 @@ void Variant_main()
         {
             std::cout << "value: " << *p1 << std::endl;
         }
+
+        // how to actively modify the members of with a visitor objects
+        std::visit( []( auto &x){
+            // fid type and make the type decay to the R value as it is a reference
+            using T = std::decay<decltype(x)>;
+            // check types abd do stuff it type
+            if constexpr (std::is_same_v<T, int>)
+            {
+                x = 1000;
+            }
+            else if constexpr (std::is_same_v<T, std::string>)
+            {
+                x = "hello";
+            }
+            else if constexpr (std::is_same_v<T, char>)
+            {
+                x = 's';
+            }
+        }, v); 
+
+        // Can also be done with a functor struct
+        std::visit(visitorFn{} , v);
     }
     catch(const std::exception& e)
     {
